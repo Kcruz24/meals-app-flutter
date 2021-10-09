279a0b8857343671fc47390f8eec96b0f291b2d0
Kevin Cruz <kocruz.24@gmail.com>
2021-10-08 20:16:50 -0400

    [162] Navigating to a New Page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
 
+import 'category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
   final String _title;
   final Color _color;
 
   CategoryItem(this._title, this._color);
 
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        _title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [_color.withOpacity(.7), _color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          _title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [_color.withOpacity(.7), _color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
